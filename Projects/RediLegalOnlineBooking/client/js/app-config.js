40b6457cf697b9cf705df0fe0fb059879be8eaa3
Truc Quynh <lovely_pinky50@yahoo.com>
2014-09-04 17:38:28 +0700

    aaa

--- Projects/RediLegalOnlineBooking/client/js/app-config.js
+++ Projects/RediLegalOnlineBooking/client/js/app-config.js
@@ -3,7 +3,7 @@
  */
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
-        .when('/', {controller: "legalBookingController", templateUrl: '/sub/rediLegalOnlineBooking/booking.html'})
+        .when('/', {controller: "legalBookingController", templateUrl: '/sub/rediLegalOnlineBooking/.html'})
         .when("/customers/add",{controller:"cusAddController",templateUrl:"/sub/helloWorld/detail.html"})
         .when("/customers/edit/:id",{controller:"cusEditController",templateUrl:"/sub/helloWorld/detail.html"})
         .when("/online-booking/booking",{controller:"legalBookingController",templateUrl:"/sub/rediLegalOnlineBooking/booking.html"})
