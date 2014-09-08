ccef810f57ac5cd6e45019f52eba458da8de0c1f
Luân Nguyễn <meditech@MEDITECHs-Mac-mini-2.local>
2014-09-08 08:55:45 +0700

    Delete redimedProjStruct

--- Projects/redimedProjStruct/client/js/controller.js
+++
@@ -1,6 +0,0 @@
-app.controller("homeController",function($scope,$rootScope,$http,$location,$window){
-    $scope.logout = function(){
-        $http.post('/users/logout');
-        $window.location.href = "/";
-    };
-});
