e68ecca5c460e83cca97680d78bd1779bb66b598
Duc Nguyen <ducntq@gmail.com>
2014-09-15 14:41:19 +0700

    Sample configuration

---
+++ Projects/redimedProjStruct/config/production.js
@@ -0,0 +1,13 @@
+// This configuration will override default configuration
+module.exports = {
+    session: {
+        secret: '3Tt79M91x8sJ63K'
+    },
+    mysql: {
+        host: '123.45.67.89',
+        user: 'username',
+        password : 'password',
+        port : 3306,
+        database:'sakila'
+    }
+};
