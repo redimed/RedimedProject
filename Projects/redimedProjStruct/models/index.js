f3171425ebb42ac9d5b758c3451575d654e49a4e
Duc Nguyen <ducntq@gmail.com>
2014-09-15 13:35:25 +0700

    Update models loader

--- Projects/redimedProjStruct/models/index.js
+++ Projects/redimedProjStruct/models/index.js
@@ -1,9 +1,13 @@
-var fs        = require('fs')
-    , path      = require('path')
-    , Sequelize = require('sequelize')
-    , lodash    = require('lodash')
-    , sequelize = new Sequelize('sequelize_test', 'root', null)
-    , db        = {};
+var fs = require('fs'),
+    path = require('path'),
+    Sequelize = require('sequelize'),
+    lodash = require('lodash'),
+    config = require('config'),
+    sequelize = new Sequelize(config.get('mysql.database'), config.get('mysql.username'), config.get('mysql.password'), {
+        host: config.get('mysql.host'),
+        port: config.get('mysql.port')
+    }),
+    db = {};
 
 fs
     .readdirSync(__dirname)
@@ -11,17 +15,17 @@ fs
         return (file.indexOf('.') !== 0) && (file !== 'index.js')
     })
     .forEach(function(file) {
-        var model = sequelize.import(path.join(__dirname, file))
+        var model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model
-    })
+    });
 
 Object.keys(db).forEach(function(modelName) {
     if ('associate' in db[modelName]) {
         db[modelName].associate(db)
     }
-})
+});
 
 module.exports = lodash.extend({
     sequelize: sequelize,
     Sequelize: Sequelize
-}, db)
+}, db);
