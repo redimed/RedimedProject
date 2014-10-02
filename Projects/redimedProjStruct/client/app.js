576e1000a28c38970738d17a5f982535c209c854
Luân Nguyễn <meditech@MEDITECHs-Mac-mini-2.local>
2014-10-02 08:52:55 +0700

    commit

--- Projects/redimedProjStruct/client/app.js
+++ Projects/redimedProjStruct/client/app.js
@@ -3,7 +3,6 @@ angular.module("app", [
     "ui.bootstrap",             // ANGULAR BOOTSTRAP
     "angularFileUpload",        // ANGULAR FILE UPLOAD
     "restangular",              // RESTANGULAR
-    "mgcrea.ngStrap",           //ANGULAR STRAP
     "ngCookies",                // ANGULAR COOKIES
     "pascalprecht.translate",   // ANGULAR TRANSLATE
     "ui.select",                // ANGULAR UI SELECT
