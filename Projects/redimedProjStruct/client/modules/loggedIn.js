9450521537f46546d6344c376258f2d90935fe56
Luân Nguyễn <meditech@MEDITECHs-Mac-mini-2.local>
2014-10-08 08:38:27 +0700

    Commit

--- Projects/redimedProjStruct/client/modules/loggedIn.js
+++ Projects/redimedProjStruct/client/modules/loggedIn.js
@@ -1,6 +1,6 @@
 angular.module("app.loggedIn",[
 	"app.loggedIn.controller", // LOGGED IN CONTROLLER
-
+    "app.lockscreen.controller", //LOCK SCREEN CONTROLLER
 	"app.loggedIn.home", // HOME MODULE
 	"app.loggedIn.user"	,	// USER MODULE
     "app.loggedIn.function", //FUNCTION MODULE
@@ -22,4 +22,13 @@ angular.module("app.loggedIn",[
 			}
 		}
 	})
+
+    .state("lockscreen",{
+            views:{
+                "root":{
+                    templateUrl: "common/views/lockscreen.html",
+                    controller:"lockscreenController"
+                }
+            }
+        })
 })
