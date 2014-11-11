50870bb042a26dca0eb77dfa9996b3d20df7d75b
kinhkyo <kinhkyo@gmail.com>
2014-11-11 14:29:47 +0700

    Commit

--- Projects/InjuryManagement/www/modules/menu/controller/menuController.js
+++ Projects/InjuryManagement/www/modules/menu/controller/menuController.js
@@ -1,6 +1,7 @@
 angular.module("starter.menu.controller",[])
-    .controller("menuController",function($scope,localStorageService){
+    .controller("menuController",function($scope,localStorageService,$state){
         $scope.logout = function(){
             localStorageService.remove('userInfo');
+            $state.go("security.login");
         }
     })
