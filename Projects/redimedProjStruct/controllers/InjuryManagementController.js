5a3e53a74099fb4da87b4b931efdefb4f886e588
luannm <luannguyen141194@gmail.com>
2014-11-16 07:11:34 +0700

    Commit

--- Projects/redimedProjStruct/controllers/InjuryManagementController.js
+++ Projects/redimedProjStruct/controllers/InjuryManagementController.js
@@ -5,10 +5,19 @@ var db = require('../models');
 module.exports = {
   search : function(req,res){
       var info = req.body.info;
-      var title = info.title;
-      var fname = info.First_name;
-      var sname = info.Sur_name;
-      var midname = info.Middle_name;
+      var title = info.Title != null ? '%'+info.Title+'%' : '%';
+      var fname = info.First_name != null ? '%'+info.First_name+'%' : '%';
+      var sname = info.Sur_name != null ? '%'+info.Sur_name+'%' : '%';
+      var midname = info.Middle_name != null ? '%'+info.Middle_name+'%' : '%';
+
+      db.sequelize.query("SELECT * FROM cln_patients WHERE Title LIKE ? AND First_name LIKE ? AND Sur_name LIKE ? AND Middle_name LIKE ?",null,[title,fname,sname,midname])
+          .success(function(data){
+              res.json(data);
+          })
+          .error(function(err){
+              res.json('err');
+              console.log(err);
+          })
 
   }
 };
