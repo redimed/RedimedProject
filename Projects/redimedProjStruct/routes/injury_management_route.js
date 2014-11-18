20a724ccd86b10958e4fd3c4c86150dd4e8b8a20
luannm <luannguyen141194@gmail.com>
2014-11-18 12:29:43 +0700

    Commit

--- Projects/redimedProjStruct/routes/injury_management_route.js
+++ Projects/redimedProjStruct/routes/injury_management_route.js
@@ -2,8 +2,12 @@
  * Created by Luan Nguyen on 11/15/2014.
  */
 var injuryController =  require('./controllers/InjuryManagementController');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 app.post('/api/im/patients/search',injuryController.search);
 app.post('/api/im/patients/getById',injuryController.getById);
 app.post('/api/im/submit',injuryController.submitInjury);
 app.post('/api/im/patients/checkMobile',injuryController.checkMobile);
+app.post('/api/im/patients/checkEmail',injuryController.checkEmail);
+app.post('/api/im/upload',multipartMiddleware,injuryController.uploadInjuryPic);
