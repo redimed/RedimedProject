0c9d3031b8d298b54d2e1ee09ffe3ff6dbfe2ec5
Tan Nguyen <tannv.dts@gmail.com>
2014-11-30 14:34:10 +0700

    update

--- Projects/redimedProjStruct/client/modules/rediLegalOnlineBooking/controllers/rlob_admin_documentStatusSummaryController.js
+++ Projects/redimedProjStruct/client/modules/rediLegalOnlineBooking/controllers/rlob_admin_documentStatusSummaryController.js
@@ -13,88 +13,102 @@ angular.module('app.loggedIn.rlob.adminDocumentStatusSummary.controller',[])
 
         var dateStartWeek=rlobDate.getDateStartWeek(new Date());
         var dateEndWeek=rlobDate.getDateEndWeek(new Date());
-        rlobService.getDocumentStatusSummary(dateStartWeek.format("YYYY/MM/DD"),dateEndWeek.format("YYYY/MM/DD")).then(function(data){
 
-            if(data.status=='success')
-            {
-                for(var i=0;i<data.data.length;i++)
+        $scope.nextWeek=function(){
+            $scope.listDaysOfWeek={};
+            $scope.listHoursInDay={};
+            $scope.listDays={};
+            dateStartWeek=dateStartWeek.add(7,'days');
+            dateEndWeek=dateEndWeek.add(7,'days');
+            $scope.getDocumentStatusSummaryInWeek(dateStartWeek,dateEndWeek);
+
+        }
+
+        $scope.previousWeek=function(){
+            $scope.listDaysOfWeek={};
+            $scope.listHoursInDay={};
+            $scope.listDays={};
+            dateStartWeek=dateStartWeek.subtract(7,'days');
+            dateEndWeek=dateEndWeek.subtract(7,'days');
+            $scope.getDocumentStatusSummaryInWeek(dateStartWeek,dateEndWeek);
+        }
+
+
+        /***
+         * Get document Status in week
+         * tannv.dts@gmail.com
+         */
+        $scope.getDocumentStatusSummaryInWeek=function(dateStart,dateEnd)
+        {
+            rlobService.getDocumentStatusSummary(dateStart.format("YYYY/MM/DD"),dateEnd.format("YYYY/MM/DD")).then(function(data){
+
+                if(data.status=='success')
                 {
-                    //get list days of weeks
-                    var item=data.data[i];
-                    var appointmentDate=new Date (data.data[i].APPOINTMENT_DATE);
-                    var dayValue=appointmentDate.getDay();
-                    if(!$scope.listDaysOfWeek[ rlobDate.daysOfWeek[dayValue].value ])
+                    for(var i=0;i<data.data.length;i++)
                     {
-                        $scope.listDaysOfWeek[ rlobDate.daysOfWeek[dayValue].value ]=rlobDate.daysOfWeek[dayValue];
-                    }
-                    //get list hours of day
-                    if(!$scope.listHoursInDay[moment(appointmentDate).format("HH:mm")])
-                    {
-                        $scope.listHoursInDay[moment(appointmentDate).format("HH:mm")]={};
-                    }
-                }
-
-
-
-                for(var i=0;i<data.data.length;i++)
-                {
-                    var item=data.data[i];
-                    var appointmentDate=new Date (data.data[i].APPOINTMENT_DATE);
-                    var dayValue=appointmentDate.getDay();
-
-                    if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value])
-                    {
-                        $scope.listDays[ rlobDate.daysOfWeek[dayValue].value]={
-                            DISPLAY:rlobDate.daysOfWeek[dayValue].display,
-                            LIST:angular.copy($scope.listHoursInDay)};
-                    }
-
-                    if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")])
-                    {
-                        $scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")]={};
-                    }
-
-                    var styleClass='';
-                    switch(item.DOCUMENT_STATUS)
-                    {
-                        case rlobConstant.documentStatus.notConfirmed:
-                            styleClass=rlobConstant.documentStatusSummaryStyleClass.notConfirmed;
-                            break;
-                        case rlobConstant.documentStatus.checked:
-                            styleClass=rlobConstant.documentStatusSummaryStyleClass.checked;
-                            break;
-                        case rlobConstant.documentStatus.noDocuments:
-                            styleClass=rlobConstant.documentStatusSummaryStyleClass.noDocuments;
-                            break;
-                    }
-                    if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")][item.BOOKING_ID])
-                    {
-                        $scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")][item.BOOKING_ID]
-                        ={
-                            BOOKING_ID:item.BOOKING_ID,
-                            WRK_SURNAME:item.WRK_SURNAME,
-                            DOCUMENT_STATUS:item.DOCUMENT_STATUS,
-                            STYLE_CLASS:styleClass
+                        //get list days of weeks
+                        var item=data.data[i];
+                        var appointmentDate=new Date (data.data[i].APPOINTMENT_DATE);
+                        var dayValue=appointmentDate.getDay();
+                        if(!$scope.listDaysOfWeek[ rlobDate.daysOfWeek[dayValue].value ])
+                        {
+                            $scope.listDaysOfWeek[ rlobDate.daysOfWeek[dayValue].value ]=rlobDate.daysOfWeek[dayValue];
+                        }
+                        //get list hours of day
+                        if(!$scope.listHoursInDay[moment(appointmentDate).format("HH:mm")])
+                        {
+                            $scope.listHoursInDay[moment(appointmentDate).format("HH:mm")]={};
                         }
                     }
 
-//                    var hours=angular.copy($scope.listHoursInDay);
-//                    hours[moment(appointmentDate).format("HH:mm")]=moment(appointmentDate).format("HH:mm");
 
 
+                    for(var i=0;i<data.data.length;i++)
+                    {
+                        var item=data.data[i];
+                        var appointmentDate=new Date (data.data[i].APPOINTMENT_DATE);
+                        var dayValue=appointmentDate.getDay();
 
+                        if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value])
+                        {
+                            $scope.listDays[ rlobDate.daysOfWeek[dayValue].value]={
+                                DISPLAY:rlobDate.daysOfWeek[dayValue].display,
+                                LIST:angular.copy($scope.listHoursInDay)};
+                        }
 
-//                    $scope.listHours[ rlobDate.daysOfWeek[dayValue].alias ].push({
-//                        BOOKING_ID:item.BOOKING_ID,
-//                        WRK_SURNAME:item.WRK_SURNAME,
-//                        DAY:rlobDate.daysOfWeek[dayValue].display,
-//                        HOURS:hours
-//                    })
+                        if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")])
+                        {
+                            $scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")]={};
+                        }
 
+                        var styleClass='';
+                        switch(item.DOCUMENT_STATUS)
+                        {
+                            case rlobConstant.documentStatus.notConfirmed:
+                                styleClass=rlobConstant.documentStatusSummaryStyleClass.notConfirmed;
+                                break;
+                            case rlobConstant.documentStatus.checked:
+                                styleClass=rlobConstant.documentStatusSummaryStyleClass.checked;
+                                break;
+                            case rlobConstant.documentStatus.noDocuments:
+                                styleClass=rlobConstant.documentStatusSummaryStyleClass.noDocuments;
+                                break;
+                        }
+                        if(!$scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")][item.BOOKING_ID])
+                        {
+                            $scope.listDays[ rlobDate.daysOfWeek[dayValue].value].LIST[moment(appointmentDate).format("HH:mm")][item.BOOKING_ID]
+                                ={
+                                BOOKING_ID:item.BOOKING_ID,
+                                WRK_SURNAME:item.WRK_SURNAME,
+                                DOCUMENT_STATUS:item.DOCUMENT_STATUS,
+                                STYLE_CLASS:styleClass
+                            }
+                        }
+                    }
                 }
+            })
+        }
+        $scope.getDocumentStatusSummaryInWeek(dateStartWeek,dateEndWeek);
 
-            }
-//            exlog.alert($scope.listDays);
-            //exlog.alert($scope.listBookingsOfDates);
-        })
+
     });
