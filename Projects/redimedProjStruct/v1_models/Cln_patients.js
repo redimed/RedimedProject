044228b432b603bc94c20aee51c06d7d8f9f31df
nguyenngockhank <nguyenngockhank@gmail.com>
2014-12-02 10:06:02 +0700

    cm

--- Projects/redimedProjStruct/v1_models/Cln_patients.js
+++ Projects/redimedProjStruct/v1_models/Cln_patients.js
@@ -5,6 +5,8 @@ install_model.sql_insert_patient_company = function(patient_id, company_id) {
 	var insert_query = install_model._squel.insert().into('patient_companies');
 	insert_query.set('company_id', company_id);
 	insert_query.set('patient_id', patient_id);
+	insert_query.set('creation_date', 'NOW()', { dontQuote: true });
+	insert_query.set('last_update_date', 'NOW()', { dontQuote: true });
 	return insert_query.toString();
 };
 
