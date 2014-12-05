3026d33eec33b9f67d4399f365b4559a494fd534
huynguyen1012 <nguyenhuy@Nguyens-MacBook-Pro.local>
2014-12-05 14:55:32 +0700

    commit

---
+++ Projects/InjuryManagement/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/nguyenhuy/RedimedProject/Projects/InjuryManagement/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/nguyenhuy/RedimedProject/Projects/InjuryManagement/platforms/android/CordovaLib/AndroidManifest.xml \
