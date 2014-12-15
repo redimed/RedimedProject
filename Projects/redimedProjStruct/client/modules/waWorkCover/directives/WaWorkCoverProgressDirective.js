448aaecb5ff54e31366f1ef6f55edc9f9e1ca819
Minh Hikari <rockmanexe1994@gmail.com>
2014-12-15 16:05:53 +0700

    WAWorkcover Update
    - Add data dependencies for all WA Workcover.

--- Projects/redimedProjStruct/client/modules/waWorkCover/directives/WaWorkCoverProgressDirective.js
+++ Projects/redimedProjStruct/client/modules/waWorkCover/directives/WaWorkCoverProgressDirective.js
@@ -123,6 +123,159 @@ angular.module('app.loggedIn.waworkcover.progress.directive', [])
                     var printId = scope.waprogress.progress_id;
                     WaWorkCoverService.progressprint(printId).then(function (result) {})
                 }
+
+
+                //Data dependencies
+                scope.$watch('waprogress.isFullCapacity', function (fullCapa) {
+                    if (fullCapa !== '1') {
+                        scope.waprogress.fullCapaFrom = null;
+                        scope.waprogress.isRequireTreat = null;
+                    }
+                })
+
+                scope.$watch('waprogress.isSomeCapacity', function (someCapa) {
+                    if (someCapa !== '1') {
+                        scope.waprogress.someCapaFrom = null;
+                        scope.waprogress.someCapaTo = null;
+                        scope.waprogress.isPreDuties = null;
+                        scope.waprogress.isModiDuties = null;
+                        scope.waprogress.isWorkModifi = null;
+                        scope.waprogress.isPreHours = null;
+                        scope.waprogress.isModiHours = null;
+                    }
+                })
+
+                scope.$watch('waprogress.isModiHours', function (isModi) {
+                    if (isModi !== '1') {
+                        scope.waprogress.modiHrs = null;
+                        scope.waprogress.modiDays = null;
+                    }
+                })
+
+                scope.$watch('waprogress.modiHrs', function (modiHrs) {
+                    if (modiHrs !== undefined && modiHrs !== null && modiHrs !== '' && scope.waprogress.modiDays !== undefined && scope.waprogress.modiDays !== null && scope.waprogress.modiDays !== '') {
+                        if (scope.waprogress.isModiHours !== '1')
+                            scope.waprogress.isModiHours = '1';
+                    }
+                })
+
+                scope.$watch('waprogress.modiDays', function (modiDays) {
+                    if (modiDays !== undefined && modiDays !== null && modiDays !== '' && scope.waprogress.modiHrs !== undefined && scope.waprogress.modiHrs !== null && scope.waprogress.modiHrs !== '') {
+                        if (scope.waprogress.isModiHours !== '1')
+                            scope.waprogress.isModiHours = '1';
+                    }
+                })
+
+                scope.$watch('waprogress.isNoCapacity', function (noCapa) {
+                    if (noCapa !== '1') {
+                        scope.waprogress.noCapaFrom = null;
+                        scope.waprogress.noCapaTo = null;
+                    }
+                })
+
+                scope.$watch('waprogress.isLiftUp', function (isLiftUp) {
+                    if (isLiftUp !== '1') {
+                        scope.waprogress.liftUpKg = null;
+                    }
+                })
+                scope.$watch('waprogress.liftUpKg', function (liftUpKg) {
+                    if (liftUpKg !== undefined && liftUpKg !== null && liftUpKg !== '') {
+                        scope.waprogress.isLiftUp = '1';
+                    } else {
+                        scope.waprogress.isLiftUp = '0';
+                    }
+                })
+
+                scope.$watch('waprogress.isSitUp', function (isSitUp) {
+                    if (isSitUp !== '1') {
+                        scope.waprogress.sitUpMins = null;
+                    }
+                })
+                scope.$watch('waprogress.sitUpMins', function (sitUpMins) {
+                    if (sitUpMins !== undefined && sitUpMins !== null && sitUpMins !== '') {
+                        scope.waprogress.isSitUp = '1';
+                    } else {
+                        scope.waprogress.isSitUp = '0';
+                    }
+
+                })
+
+                scope.$watch('waprogress.isStandUp', function (isStandUp) {
+                    if (isStandUp !== '1') {
+                        scope.waprogress.standUpMins = null;
+                    }
+                })
+                scope.$watch('waprogress.standUpMins', function (standUpMins) {
+                    if (standUpMins !== undefined && standUpMins !== null && standUpMins !== '') {
+                        scope.waprogress.isStandUp = '1';
+                    } else {
+                        scope.waprogress.isStandUp = '0';
+                    }
+                })
+
+
+                scope.$watch('waprogress.isWalkUp', function (isWalkUp) {
+                    if (isWalkUp !== '1') {
+                        scope.waprogress.walkUpMeter = null;
+                    }
+                })
+                scope.$watch('waprogress.walkUpMeter', function (walkUpMeter) {
+                    if (walkUpMeter !== undefined && walkUpMeter !== null && walkUpMeter !== '') {
+                        scope.waprogress.isWalkUp = '1';
+                    } else {
+                        scope.waprogress.isWalkUp = '0';
+                    }
+                })
+
+                scope.$watch('waprogress.activities_1', function (activities_1) {
+                    if (activities_1 === null || activities_1 === undefined || activities_1 === '') {
+                        scope.waprogress.outcome_1 = null;
+                        scope.waprogress.isRequired_1 = null;
+                    }
+                })
+                scope.$watch('waprogress.activities_2', function (activities_2) {
+                    if (activities_2 === null || activities_2 === undefined || activities_2 === '') {
+                        scope.waprogress.outcome_2 = null;
+                        scope.waprogress.isRequired_2 = null;
+                    }
+                })
+                scope.$watch('waprogress.activities_3', function (activities_3) {
+                    if (activities_3 === null || activities_3 === undefined || activities_3 === '') {
+                        scope.waprogress.outcome_3 = null;
+                        scope.waprogress.isRequired_3 = null;
+                    }
+                })
+                scope.$watch('waprogress.activities_4', function (activities_4) {
+                    if (activities_4 === null || activities_4 === undefined || activities_4 === '') {
+                        scope.waprogress.outcome_4 = null;
+                        scope.waprogress.isRequired_4 = null;
+                    }
+                })
+                scope.$watch('waprogress.activities_5', function (activities_5) {
+                    if (activities_5 === null || activities_5 === undefined || activities_5 === '') {
+                        scope.waprogress.outcome_5 = null;
+                        scope.waprogress.isRequired_5 = null;
+                    }
+                })
+                scope.$watch('waprogress.activities_6', function (activities_6) {
+                    if (activities_6 === null || activities_6 === undefined || activities_6 === '') {
+                        scope.waprogress.outcome_6 = null;
+                        scope.waprogress.isRequired_6 = null;
+                    }
+                })
+
+                scope.$watch('waprogress.isReview', function (isReview) {
+                    if (isReview !== '1') {
+                        scope.waprogress.reviewOn = null;
+                    }
+                })
+
+                scope.$watch('waprogress.reviewOn', function (reviewOn) {
+                    if (reviewOn !== undefined && reviewOn !== null && reviewOn !== '') {
+                        scope.waprogress.isReview = '1';
+                    }
+                })
+
             }
         }
     });
