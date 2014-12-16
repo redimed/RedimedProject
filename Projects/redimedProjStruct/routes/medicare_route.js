3e2e578c16fb49dcd98f80c9f86df010ad5541a2
redimed <pnguyen@redimed.com.au>
2014-12-16 10:24:45 +0800

    sa

--- Projects/redimedProjStruct/routes/medicare_route.js
+++ Projects/redimedProjStruct/routes/medicare_route.js
@@ -1,8 +1,7 @@
 /**
  * Created by Luan Nguyen on 12/12/2014.
  */
- /*
+ 
 var MedicareController = require('./controllers/HICMedicareController');
 
 app.post('/api/medicare/verify',MedicareController.verify);
-*/
