1db8ad46291d4c37b9e4dedf6f2db4befe2fbc9a
nguyenngockhank <nguyenngockhank@gmail.com>
2015-01-07 08:37:07 +0700

    cmmmit

--- Projects/redimedProjStruct/client/modules/doctor/controllers/DoctorPatientsDetailController.js
+++ Projects/redimedProjStruct/client/modules/doctor/controllers/DoctorPatientsDetailController.js
@@ -3,8 +3,11 @@ angular.module("app.loggedIn.doctor.patients.detail.controller", [
 ]).controller("DoctorPatientsDetailController", function ($scope, $state, $cookieStore, DoctorService, PatientService, localStorageService) {
   
     $scope.goToApptDetail = function (row) {
-		localStorageService.set("apptTempInfo", {CAL_ID: row.CAL_ID});
-        $state.go("loggedIn.doctor.patients.detail.appt");
+        var cal_id = row.CAL_ID;
+        var patient_id = $scope.patient.Patient_id;
+        $state.go("loggedIn.patient.appointment", {patient_id: patient_id, cal_id: cal_id});
+		// localStorageService.set("apptTempInfo", {CAL_ID: row.CAL_ID});
+  //       $state.go("loggedIn.doctor.patients.detail.appt");
     }
 
     var doctorInfo = $cookieStore.get("doctorInfo");
