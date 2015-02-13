785f3bec62358ed5b9faa03ce5143bdc81fb09da
nguyenngockhank <nguyenngockhank@gmail.com>
2015-02-13 11:39:30 +0700

    commit

--- Projects/redimedProjStruct/client/modules/doctor/controllers/DoctorPaperlessController.js
+++ Projects/redimedProjStruct/client/modules/doctor/controllers/DoctorPaperlessController.js
@@ -1,26 +1,16 @@
 angular.module("app.loggedIn.doctor.paperless.controller", [])
 
-.controller("DoctorPaperlessController", function($scope, PatientService, $stateParams, localStorageService){
-	$scope.current_patient = {};
+.controller("DoctorPaperlessController", function($scope, DocumentService, PatientService, $stateParams, localStorageService){
+	var patient_id = $stateParams.patient_id;
 
-	// Init Object
-	var initObject = function(){
-		PatientService.mdtById($stateParams.patient_id).then(function(response){	
-			$scope.current_patient = response.data;
 
-			for(var key in $scope.current_patient){
-				if($scope.current_patient[key]){
-					if(key.indexOf("is") != -1 || key.indexOf("Is") != -1)
-						$scope.current_patient[key] = $scope.current_patient[key].toString();
-					if(key.indexOf("_date") != -1 || key.indexOf("DOB") != -1 || key.indexOf("Exp") != -1)
-						$scope.current_patient[key] = new Date($scope.current_patient[key]);
-				}
-			}
+	$scope.OptionGorgonMA = DocumentService.optionGorgonMA(patient_id);
 
-			localStorageService.set('tempPatient', $scope.current_patient);
-		})
-	}
+	$scope.OptionGorgonFA = DocumentService.optionGorgonFA(patient_id);
 
-	initObject();
-	// End Init Object
+	$scope.OptionGorgonUQ = DocumentService.optionGorgonUQ(patient_id);
+
+	$scope.OptionGorgonMH = DocumentService.optionGorgonMH(patient_id);
+
+
 })
