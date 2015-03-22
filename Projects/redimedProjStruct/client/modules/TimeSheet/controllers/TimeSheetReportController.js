a5bb3b961e282cafb342f3b6b2c658ad8e812c11
thanh1101681 <thanh1101681@gmail.com>
2015-03-22 07:54:46 +0700

    commit

--- Projects/redimedProjStruct/client/modules/TimeSheet/controllers/TimeSheetReportController.js
+++ Projects/redimedProjStruct/client/modules/TimeSheet/controllers/TimeSheetReportController.js
@@ -1,4 +1,4 @@
 angular.module("app.loggedIn.TimeSheet.Report.Controller", [])
-    .controller("ReportController", function() {
-
+    .controller("ReportController", function($scope, localStorageService) {
+        $scope.position = localStorageService.get('position');
     });
