6981cca962980c7ec29b2669b1ad7f3f78416b61
luannm <luannguyen141194@gmail.com>
2015-04-09 08:16:57 +0700

    commit

--- Projects/redimedProjStruct/models/IMInjuryImage.js
+++ Projects/redimedProjStruct/models/IMInjuryImage.js
@@ -3,11 +3,11 @@
  */
 module.exports = function(sequelize,DataTypes){
     var IMInjuryImage = sequelize.define('IMInjuryImage',{
-        injury_image_id : {type:DataTypes.INTEGER(11), primaryKey:true} ,
+        id : {type:DataTypes.INTEGER(11), primaryKey:true} ,
         injury_id : DataTypes.INTEGER(11) ,
-        image : DataTypes.TEXT,
+        injury_part: DataTypes.STRING(200),
         description : DataTypes.TEXT,
-        STATUS : DataTypes.STRING(100) ,
+        img_url : DataTypes.TEXT,
         Created_by : DataTypes.INTEGER(11) ,
         Creation_date : DataTypes.DATE ,
         Last_updated_by : DataTypes.INTEGER(11) ,
