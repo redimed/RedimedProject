e97f20f863811529259909a729b380e36b3fc76c
luannm <luannguyen141194@gmail.com>
2015-05-22 09:14:01 +0700

    commit

--- Projects/redimedProjStruct/socket.js
+++ Projects/redimedProjStruct/socket.js
@@ -306,55 +306,55 @@ module.exports = function(io,cookie,cookieParser) {
                 })
         })
 
-        socket.on('disconnect', function (reason) {
+        // socket.on('disconnect', function (reason) {
 
-            db.User.find({where:{socket:socket.id}},{raw:true})
-                .success(function(user){
-                    if(user)
-                    {
-                        db.UserType.find({where:{ID:user.user_type}},{raw:true})
-                            .success(function(type){
-                                if(type.user_type == 'Driver')
-                                    io.sockets.emit('driverLogout',user.id);
+        //     db.User.find({where:{socket:socket.id}},{raw:true})
+        //         .success(function(user){
+        //             if(user)
+        //             {
+        //                 db.UserType.find({where:{ID:user.user_type}},{raw:true})
+        //                     .success(function(type){
+        //                         if(type.user_type == 'Driver')
+        //                             io.sockets.emit('driverLogout',user.id);
 
-                                if(user.socket == socket.id)
-                                {
-                                     db.sequelize.query("UPDATE `users` SET `socket` = NULL, socketMobile = NULL WHERE socket = ?", null, {raw: true}, [socket.id])
-                                        .success(function () {
-                                            getOnlineUser();
-                                        })
-                                        .error(function (err) {
-                                            console.log(err);
-                                        })
-                                }
+        //                         if(user.socket == socket.id)
+        //                         {
+        //                              db.sequelize.query("UPDATE `users` SET `socket` = NULL, socketMobile = NULL WHERE socket = ?", null, {raw: true}, [socket.id])
+        //                                 .success(function () {
+        //                                     getOnlineUser();
+        //                                 })
+        //                                 .error(function (err) {
+        //                                     console.log(err);
+        //                                 })
+        //                         }
 
-                                if(user.socketMobile == socket.id)
-                                {
-                                    db.sequelize.query("UPDATE `users` SET socketMobile = NULL WHERE socketMobile = ?", null, {raw: true}, [socket.id])
-                                        .success(function () {
-                                            getOnlineUser();
-                                        })
-                                        .error(function (err) {
-                                            console.log(err);
-                                        })
-                                }
+        //                         if(user.socketMobile == socket.id)
+        //                         {
+        //                             db.sequelize.query("UPDATE `users` SET socketMobile = NULL WHERE socketMobile = ?", null, {raw: true}, [socket.id])
+        //                                 .success(function () {
+        //                                     getOnlineUser();
+        //                                 })
+        //                                 .error(function (err) {
+        //                                     console.log(err);
+        //                                 })
+        //                         }
 
 
                                
-                            })
-                            .error(function (err) {
-                                console.log(err);
-                            })
-                    }
+        //                     })
+        //                     .error(function (err) {
+        //                         console.log(err);
+        //                     })
+        //             }
 
-                })
-                .error(function (err) {
-                    console.log(err);
-                })
+        //         })
+        //         .error(function (err) {
+        //             console.log(err);
+        //         })
 
-                socket.removeAllListeners();
+        //         socket.removeAllListeners();
 
-        });
+        // });
 
 
         function getOnlineUser(){
