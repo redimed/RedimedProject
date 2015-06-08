46fed74a01054e61518b276a60a61a9627dc5d75
Tan Nguyen <tannv.dts@gmail.com>
2015-06-08 16:23:43 +0800

    commit

--- Projects/redimedProjStruct/client/modules/security/controllers/SecurityLoginController.js
+++ Projects/redimedProjStruct/client/modules/security/controllers/SecurityLoginController.js
@@ -76,60 +76,60 @@ angular.module("app.security.login.controller",[
             socket.on('login_success',function(){
                 UserService.detail().then(function (response) {
               
-                if (typeof response.userInfo !== 'undefined') {
-                        $cookieStore.put("userInfo", response.userInfo);
-                        $cookieStore.put("isRemember",$scope.modelUser.isRemember);
+                    if (typeof response.userInfo !== 'undefined') {
+                            $cookieStore.put("userInfo", response.userInfo);
+                            $cookieStore.put("isRemember",$scope.modelUser.isRemember);
 
-                        if (typeof response.companyInfo !== 'undefined')
-                            $cookieStore.put("companyInfo", response.companyInfo);
-
-
-                        if (response.userInfo.UserType.user_type == 'Doctor') {
-                            DoctorService.getByUserId(response.userInfo.id).then(function (data) {
-                                if (data) {
-                                    $cookieStore.put('doctorInfo', {
-                                        doctor_id: data.doctor_id,
-                                        NAME: data.NAME,
-                                        Provider_no: data.Provider_no,
-                                        CLINICAL_DEPT_ID: data.CLINICAL_DEPT_ID
-                                    });
-                                }
-                            });
-                        }
-
-                        if (response.userInfo['function_id'] != null) {
-                            UserService.getFunction(response.userInfo['function_id']).then(function (data) {
-                                if(typeof data.definition !== 'undefined')
-                                {
-                                    var rs = data.definition.split('(');
-                                    if (rs[0] != null) {
-                                        if (rs[1] != null) {
-                                            var r = rs[1].split(')');
-                                            var params = eval("(" + r[0] + ")");
+                            if (typeof response.companyInfo !== 'undefined')
+                                $cookieStore.put("companyInfo", response.companyInfo);
 
 
-                                            $state.go(rs[0], params, {location: "replace", reload: true});
-                                        }
-                                        else {
-                                            $state.go(rs[0], {location: "replace", reload: true});
+                            if (response.userInfo.UserType.user_type == 'Doctor') {
+                                DoctorService.getByUserId(response.userInfo.id).then(function (data) {
+                                    if (data) {
+                                        $cookieStore.put('doctorInfo', {
+                                            doctor_id: data.doctor_id,
+                                            NAME: data.NAME,
+                                            Provider_no: data.Provider_no,
+                                            CLINICAL_DEPT_ID: data.CLINICAL_DEPT_ID
+                                        });
+                                    }
+                                });
+                            }
+
+                            if (response.userInfo['function_id'] != null) {
+                                UserService.getFunction(response.userInfo['function_id']).then(function (data) {
+                                    if(typeof data.definition !== 'undefined')
+                                    {
+                                        var rs = data.definition.split('(');
+                                        if (rs[0] != null) {
+                                            if (rs[1] != null) {
+                                                var r = rs[1].split(')');
+                                                var params = eval("(" + r[0] + ")");
+
+
+                                                $state.go(rs[0], params, {location: "replace", reload: true});
+                                            }
+                                            else {
+                                                $state.go(rs[0], {location: "replace", reload: true});
+                                            }
                                         }
                                     }
-                                }
-                                else
-                                {
-                                     $state.go('loggedIn.home',null,{location: "replace", reload: true});
-                                }
-                            })
-                        }
-                        else {
-                            $state.go('loggedIn.home',null,{location: "replace", reload: true});
-                        }
+                                    else
+                                    {
+                                         $state.go('loggedIn.home',null,{location: "replace", reload: true});
+                                    }
+                                })
+                            }
+                            else {
+                                $state.go('loggedIn.home',null,{location: "replace", reload: true});
+                            }
+                        
+
+
+                    }
                     
-
-
-                }
-                
-            });
+                });
             });
             
         }
