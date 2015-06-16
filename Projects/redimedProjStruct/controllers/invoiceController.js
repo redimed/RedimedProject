3e3ef8b92f985b0d77cbc59d71741b75c26fe2ef
Tan Nguyen <tannv.dts@gmail.com>
2015-06-16 16:53:18 +0800

    commmit

---
+++ Projects/redimedProjStruct/controllers/invoiceController.js
@@ -0,0 +1,18 @@
+/**
+ * created by: tan, manh
+ * creation date: 16-6-2015
+ */
+
+var kiss=require('./kissUtilsController');
+var moment=require('moment');
+var errorCode=require('./errorCode');
+
+var controllerCode="RED_INVOICE_V3"
+module.exports =
+{
+	aa:function(req,res)
+	{
+		var fHeader="ConsultationController->getPatientCompany";
+    	var functionCode="FN002";
+	}
+}
