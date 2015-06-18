3d9b9c32fe88aacc563db55a5658b0c62f4c3361
phu003 <phanphu003@gmail.com>
2015-06-18 17:40:23 +0700

    commit

--- Projects/redimedProjStruct/client/modules/script/controllers/ScriptAddController.js
+++ Projects/redimedProjStruct/client/modules/script/controllers/ScriptAddController.js
@@ -4,7 +4,7 @@ angular.module('app.loggedIn.script.add.controller', [])
 	  success: false,
 	  medicare:medicare
 	}
-	//console.log('_________',  $scope.scriptadd.medicare);
+	console.log('_________',  medicare);
 	$scope.$watch('scriptadd.success', function(success){
 		  if(success){
 		   $modalInstance.close($scope.scriptadd);
