eba579bd08c34f647caf430bd572973683a23d6d
Huy Nguyễn <nnh1012@gmail.com>
2015-06-18 14:42:50 +0700

    commit

--- Projects/InjuryManagement/www/modules/security/controller/securityLoginController.js
+++ Projects/InjuryManagement/www/modules/security/controller/securityLoginController.js
@@ -3,63 +3,9 @@ angular.module('starter.security.login.controller',[])
                                                    localStorageService, $cordovaPush, $cordovaDialogs,
                                                    $cordovaMedia, signaling, phoneCallService, $ionicPopup, $ionicLoading, $timeout){
         $scope.notifications = [];
-
-        $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
-            localStorageService.set("notificationLS", notification);
-            if (ionic.Platform.isAndroid()) {
-                handleAndroid(notification);
-            }
-            else if (ionic.Platform.isIOS()) {
-                handleIOS(notification);
-                $scope.$apply(function () {
-                    $scope.notifications.push(JSON.stringify(notification.alert));
-                })
-            }
-        });
-
-        //Android.
-        function handleAndroid(notification) {
-            if (notification.event == "registered") {
-                $scope.modelUser.token = notification.regid;
-                $scope.modelUser.platform = ionic.Platform.platform();
-            }
-        }
-
-        //iOS.
-        function handleIOS(notification) {
-            if (notification.event == "registered") {
-                $scope.modelUser.token = notification.regid;
-                $scope.modelUser.platform = ionic.Platform.platform();
-            }
-            if (notification.foreground == "1") {
-                if (notification.sound) {
-                    var mediaSrc = $cordovaMedia.newMedia(notification.sound);
-                    mediaSrc.promise.then($cordovaMedia.play(mediaSrc.media));
-                }
-
-                if (notification.body && notification.messageFrom) {
-                    $cordovaDialogs.alert(notification.body, notification.messageFrom);
-                }
-                else $cordovaDialogs.alert(notification.alert, "Push Notification Received");
-
-                if (notification.badge) {
-                    $cordovaPush.setBadgeNumber(notification.badge).then(function (result) {
-                        console.log("Set badge success " + result)
-                    }, function (err) {
-                        console.log("Set badge error " + err)
-                    });
-                }
-            }
-            else {
-                if (notification.body && notification.messageFrom) {
-                    $cordovaDialogs.alert(notification.body, "(RECEIVED WHEN APP IN BACKGROUND) " + notification.messageFrom);
-                }
-                else $cordovaDialogs.alert(notification.alert, "(RECEIVED WHEN APP IN BACKGROUND) Push Notification Received");
-            }
-        }
+        $scope.modelUser.platform = ionic.Platform.platform();
 
         signaling.removeListener('isError');
-
         signaling.on('isError', function () {
             $ionicLoading.hide();
             document.addEventListener("deviceready", function() {
@@ -93,6 +39,9 @@ angular.module('starter.security.login.controller',[])
 
         // SUBMIT LOGIN
         $scope.loginApp = function() {
+            if(ionic.Platform.isIOS()) {
+                $scope.modelUser.token = SecurityService.getIosToken();
+            }
             $scope.messageLoading = {message: "Signing..."};
             $ionicLoading.show({
                 templateUrl: "modules/loadingTemplate.html",
@@ -101,6 +50,7 @@ angular.module('starter.security.login.controller',[])
                 maxWidth: 500,
                 showDelay: 0
             });
+            console.log($scope.modelUser);
             SecurityService.login($scope.modelUser).then(function(response) {
                 signaling.emit('checkLogin', $scope.modelUser.username);
             }, function(error) {
