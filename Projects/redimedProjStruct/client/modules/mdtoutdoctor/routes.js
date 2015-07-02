b3e2515fa5a07fe11f52e66ed3e10a51f4d2c77f
ducmanh0502 <ngaokiems1@yahoo.com>
2015-07-02 11:58:01 +0700

    commit

--- Projects/redimedProjStruct/client/modules/mdtoutdoctor/routes.js
+++ Projects/redimedProjStruct/client/modules/mdtoutdoctor/routes.js
@@ -7,17 +7,41 @@ angular.module('app.loggedIn.mdtoutdoctor', [])
 			url: '/mdtoutdoctorload',
 			resolve: {
 				init: function($q, $rootScope, $state, $timeout, $ocLazyLoad){
-
-					$ocLazyLoad.load("modules/mdtoutdoctor/extend_routes.js");
-					$ocLazyLoad.load("modules/mdtoutdoctor/dialogs/MdtoutdoctorAdddialog.js");
-					$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorAddDirective.js");
-					$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorDetailDirective.js");
-					$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorDirectives.js");
-					$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorSearchDirective.js")
-
-					.then(function(){
-						$state.go('loggedIn.mdtoutdoctor_load');
-					})
+					async.waterfall([
+						function(callback) {
+							     $ocLazyLoad.load("modules/mdtoutdoctor/extend_routes.js")
+						    .then(function() {
+						        callback(null);
+						    });
+						},
+						function(callback) {
+							$ocLazyLoad.load("modules/mdtoutdoctor/dialogs/MdtoutdoctorAdddialog.js")
+						    .then(function() {
+						        callback(null);
+						    });
+						},
+						function(callback) {
+							$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorAddDirective.js")
+						    .then(function() {
+						        callback(null);
+						    });
+						},
+						function(callback) {
+							$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorDirectives.js")
+						    .then(function() {
+						        callback(null);
+						    });
+						},
+						function(callback) {
+							$ocLazyLoad.load("modules/mdtoutdoctor/directives/mdtOutdoctorSearchDirective.js")
+						    .then(function() {
+						        callback(null);
+						    });
+						},
+						function(callback) {
+							$state.go('loggedIn.mdtoutdoctor_load');
+						}
+					]);
 				}
 			}
 		})
