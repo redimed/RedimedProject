0ba5bf490d99624ffc776b80d8dfff5a58a4e52a
thanh1101681 <thanh1101681@gmail.com>
2015-07-08 00:15:08 +0700

    commit

--- Projects/redimedProjStruct/client/modules/document/document.js
+++ Projects/redimedProjStruct/client/modules/document/document.js
@@ -3,156 +3,157 @@
  */
 
 angular.module("app.loggedIn.document", [
-    "app.loggedIn.document.cat2.controllers",
-    "app.loggedIn.document.cat3.controllers",
-    "app.loggedIn.document.FA.controllers",
-    "app.loggedIn.document.MA.controllers",
-    "app.loggedIn.document.IDS.controllers",
-    "app.loggedIn.document.gorgonFA.controllers",
-    "app.loggedIn.document.gorgonMA.controllers",
-    "app.loggedIn.document.gorgonMH.controllers",
-    "app.loggedIn.document.gorgonUQ.controllers",
-    "app.loggedIn.document.services",
-    "app.loggedIn.document.MH.controllers",
-    "app.loggedIn.document.MRS.controllers",
-    "app.loggedIn.document.SA1.controllers",
-    "app.loggedIn.document.SA2.controllers",
-    "app.loggedIn.document.COE.controllers",
-    "app.loggedIn.document.demo.controllers",
-    "app.loggedIn.document.detail.controllers",
-    "app.loggedIn.document.addPatient.controllers",
-    "app.loggedIn.document.form18.controllers",
-    "app.loggedIn.document.newFA.controllers"
-])
-    .config(function ($stateProvider) {
+        "app.loggedIn.document.cat2.controllers",
+        "app.loggedIn.document.cat3.controllers",
+        "app.loggedIn.document.FA.controllers",
+        "app.loggedIn.document.MA.controllers",
+        "app.loggedIn.document.IDS.controllers",
+        "app.loggedIn.document.gorgonFA.controllers",
+        "app.loggedIn.document.gorgonMA.controllers",
+        "app.loggedIn.document.gorgonMH.controllers",
+        "app.loggedIn.document.gorgonUQ.controllers",
+        "app.loggedIn.document.services",
+        "app.loggedIn.document.MH.controllers",
+        "app.loggedIn.document.MRS.controllers",
+        "app.loggedIn.document.SA1.controllers",
+        "app.loggedIn.document.SA2.controllers",
+        "app.loggedIn.document.COE.controllers",
+        "app.loggedIn.document.demo.controllers",
+        "app.loggedIn.document.detail.controllers",
+        "app.loggedIn.document.addPatient.controllers",
+        "app.loggedIn.document.form18.controllers",
+        "app.loggedIn.document.newFA.controllers",
+        "app.loggedIn.document.ams.controllers"
+    ])
+    .config(function($stateProvider) {
         $stateProvider
 
             .state("loggedIn.demo", {
-                url: "/demo",
-                templateUrl: "modules/document/views/DemoDocument.html",
-                controller: 'demoController'
-            })
+            url: "/demo",
+            templateUrl: "modules/document/views/DemoDocument.html",
+            controller: 'demoController'
+        })
 
-            .state("loggedIn.Detail", {
-                url: "/detail",
-                templateUrl: "modules/document/views/patientDetail.html",
-                controller: 'detailController'
-            })
+        .state("loggedIn.Detail", {
+            url: "/detail",
+            templateUrl: "modules/document/views/patientDetail.html",
+            controller: 'detailController'
+        })
 
-            .state("loggedIn.addPatient", {
-                url: "/addPatient",
-                templateUrl: "modules/document/views/addPatient.html",
-                controller: 'addPatientController'
-            })
+        .state("loggedIn.addPatient", {
+            url: "/addPatient",
+            templateUrl: "modules/document/views/addPatient.html",
+            controller: 'addPatientController'
+        })
 
-            .state("loggedIn.COE", {
-                url: "/COE/:patient_id/:cal_id",
-                templateUrl: "modules/document/views/COE.html",
-                controller: 'COEController'
-            })
+        .state("loggedIn.COE", {
+            url: "/COE/:patient_id/:cal_id",
+            templateUrl: "modules/document/views/COE.html",
+            controller: 'COEController'
+        })
 
-            .state("loggedIn.category2", {
-                url: "/category2/:patient_id/:cal_id",
+        .state("loggedIn.category2", {
+            url: "/category2/:patient_id/:cal_id",
 
-                templateUrl: "modules/document/views/category2.html",
-                controller: 'Cat2Controller'
+            templateUrl: "modules/document/views/category2.html",
+            controller: 'Cat2Controller'
 
-            })
+        })
 
-            .state("loggedIn.category3", {
-                url: "/category3/:patient_id/:cal_id",
+        .state("loggedIn.category3", {
+            url: "/category3/:patient_id/:cal_id",
 
-                templateUrl: "modules/document/views/category3.html",
-                controller: 'Cat3Controller'
+            templateUrl: "modules/document/views/category3.html",
+            controller: 'Cat3Controller'
 
-            })
+        })
 
-            
 
-            .state("loggedIn.MA", {
-                url: "/MA/:patient_id/:cal_id",
-                templateUrl: "modules/document/views/medicalAssessment.html",
-                controller: 'MAController'
 
-            })
+        .state("loggedIn.MA", {
+            url: "/MA/:patient_id/:cal_id",
+            templateUrl: "modules/document/views/medicalAssessment.html",
+            controller: 'MAController'
 
-            .state("loggedIn.IDS", {
-                url: "/IDS/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/instantScreen.html",
-                controller: 'IDSController'
+        .state("loggedIn.IDS", {
+            url: "/IDS/:patient_id/:cal_id",
 
-            })
+            templateUrl: "modules/document/views/instantScreen.html",
+            controller: 'IDSController'
 
-            .state("loggedIn.gorgonFA", {
-                url: "/gorgonFA/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/gorgonFA.html",
-                controller: 'gorgonFAController'
+        .state("loggedIn.gorgonFA", {
+            url: "/gorgonFA/:patient_id/:cal_id",
 
-            })
+            templateUrl: "modules/document/views/gorgonFA.html",
+            controller: 'gorgonFAController'
 
-            .state("loggedIn.gorgonMA", {
-                url: "/gorgonMA/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/gorgonMA.html",
-                controller: 'gorgonMAController'
+        .state("loggedIn.gorgonMA", {
+            url: "/gorgonMA/:patient_id/:cal_id",
 
-            })
+            templateUrl: "modules/document/views/gorgonMA.html",
+            controller: 'gorgonMAController'
 
-            .state("loggedIn.gorgonMH", {
-                url: "/gorgonMH/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/gorgonMH.html",
-                controller: 'gorgonMHController'
+        .state("loggedIn.gorgonMH", {
+            url: "/gorgonMH/:patient_id/:cal_id",
 
-            })
+            templateUrl: "modules/document/views/gorgonMH.html",
+            controller: 'gorgonMHController'
 
-            .state("loggedIn.gorgonUQ", {
-                url: "/gorgonUQ/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/gorgonUQ.html",
-                controller: 'gorgonUQController'
+        .state("loggedIn.gorgonUQ", {
+            url: "/gorgonUQ/:patient_id/:cal_id",
 
-            })
+            templateUrl: "modules/document/views/gorgonUQ.html",
+            controller: 'gorgonUQController'
 
-            .state("loggedIn.Form18", {
-                url: "/Form18/:patient_id/:cal_id",
+        })
 
-                templateUrl: "modules/document/views/form18.html",
-                controller: 'form18Controller'
-            })
+        .state("loggedIn.Form18", {
+            url: "/Form18/:patient_id/:cal_id",
 
-            .state("loggedIn.MH", {
-                url: "/MH/:patient_id/:cal_id",
+            templateUrl: "modules/document/views/form18.html",
+            controller: 'form18Controller'
+        })
 
-                templateUrl: "modules/document/views/medicalhistory.html",
-                controller: 'MHController'
-            })
+        .state("loggedIn.MH", {
+            url: "/MH/:patient_id/:cal_id",
 
-            .state("loggedIn.MRS", {
-                url: "/MRS/:patient_id/:cal_id",
+            templateUrl: "modules/document/views/medicalhistory.html",
+            controller: 'MHController'
+        })
 
-                templateUrl: "modules/document/views/medicalresultsummary.html",
-                controller: 'MRSController'
-            })
+        .state("loggedIn.MRS", {
+            url: "/MRS/:patient_id/:cal_id",
 
-            .state("loggedIn.SA1", {
-                url: "/SA1/:patient_id/:cal_id",
+            templateUrl: "modules/document/views/medicalresultsummary.html",
+            controller: 'MRSController'
+        })
 
-                templateUrl: "modules/document/views/audiogramresults1.html",
-                controller: 'SA1Controller'
-            })
+        .state("loggedIn.SA1", {
+            url: "/SA1/:patient_id/:cal_id",
 
-            .state("loggedIn.SA2", {
+            templateUrl: "modules/document/views/audiogramresults1.html",
+            controller: 'SA1Controller'
+        })
+
+        .state("loggedIn.SA2", {
                 url: "/SA2/:patient_id/:cal_id",
 
                 templateUrl: "modules/document/views/audiogramresults2.html",
                 controller: 'SA2Controller'
             })
-            .state("loggedIn.FA",{
+            .state("loggedIn.FA", {
                 // url:"/newFA/:patient_id/:cal_id/:fa_id",
-                url:"/FA/:patient_id/:cal_id/:fa_id",
+                url: "/FA/:patient_id/:cal_id/:fa_id",
                 templateUrl: "modules/document/views/newFunctionalAssessment.html",
                 controller: "newFAController"
             })
@@ -162,6 +163,11 @@ angular.module("app.loggedIn.document", [
                 templateUrl: "modules/document/views/functionAssessment.html",
                 controller: 'FAController'
 
+            })
+            .state("loggedIn.ams", {
+                url: "/ams/:patient_id/:cal_id",
+                templateUrl: "modules/document/views/ams.html",
+                controller: "amsController"
             })
 
 
