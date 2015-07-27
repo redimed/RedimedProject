098033808388f06783ebc86bab21b8e1f512c9a8
ducmanh0502 <ngaokiems1@yahoo.com>
2015-07-27 10:49:36 +0700

    commit required source start date

--- Projects/redimedProjStruct/client/modules/item/js/item_helper.js
+++ Projects/redimedProjStruct/client/modules/item/js/item_helper.js
@@ -1,10 +1,10 @@
 var itemConst={
 	feeGroupType:{
-		private_fund:{value:'private_fund',display:'Private Fund',sourceFileTypes:{txt:{value:'txt',display:'txt'}}},//tannv: giu nguyen group type cu
-		medicare:{value:'medicare',display:'Medicare',sourceFileTypes:{xml:{value:'xml',display:'xml'}}},//tannv: chuyen tu type 'item_fee_type' sang 'Medicare'
-		dva:{value:'dva',display:'DVA',sourceFileTypes:{xml:{value:'xml',display:'xml'}}},//tannv: add
-		public_hospital:{value:'public_hospital',display:'Public Hospital'},//tannv: add
-		workcover:{value:'workcover',display:'Workcover'}//tan add
+		private_fund:{value:'private_fund',display:'Private Fund',haveStartDate:true,sourceFileTypes:{txt:{value:'txt',display:'txt'}}},//tannv: giu nguyen group type cu
+		medicare:{value:'medicare',display:'Medicare',haveStartDate:false,sourceFileTypes:{xml:{value:'xml',display:'xml'}}},//tannv: chuyen tu type 'item_fee_type' sang 'Medicare'
+		dva:{value:'dva',display:'DVA',haveStartDate:true,sourceFileTypes:{xml:{value:'xml',display:'xml'}}},//tannv: add
+		public_hospital:{value:'public_hospital',haveStartDate:false,display:'Public Hospital'},//tannv: add
+		workcover:{value:'workcover',haveStartDate:false,display:'Workcover'}//tan add
 	},
 	//tannv
 	sourceFileType:{
@@ -13,4 +13,3 @@ var itemConst={
 		xls:'xls'
 	}
 }
-
