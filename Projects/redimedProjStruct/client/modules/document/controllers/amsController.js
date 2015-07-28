6d7a4cdbeef8de521165bbff419c22be34727011
thanh1101681 <thanh1101681@gmail.com>
2015-07-28 10:55:39 +0700

    commit

--- Projects/redimedProjStruct/client/modules/document/controllers/amsController.js
+++ Projects/redimedProjStruct/client/modules/document/controllers/amsController.js
@@ -1,4 +1,4 @@
 angular.module("app.loggedIn.document.ams.controllers", [])
     .controller("amsController", function($scope, $cookieStore) {
-        console.log('go to asm controller');
+        
     });
