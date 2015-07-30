c7ec773d4d2ecf130301ccc7871992f3c1ef7a82
thanh1101681 <thanh1101681@gmail.com>
2015-07-30 14:13:52 +0700

    commit 50% validate

--- Projects/redimedProjStruct/client/modules/document/controllers/amsController.js
+++ Projects/redimedProjStruct/client/modules/document/controllers/amsController.js
@@ -1,4 +1,31 @@
 angular.module("app.loggedIn.document.ams.controllers", [])
     .controller("amsController", function($scope, $cookieStore) {
-        
+        $scope.info = {};
+        $scope.ESQMode = [{
+            code: 0,
+            name: "0"
+        }, {
+            code: 1,
+            name: "1"
+        }, {
+            code: 2,
+            name: "2"
+        }, {
+            code: 3,
+            name: "3"
+        }];
+        var patientID = 104,
+            calID = 51;
     });
+//CHECK HEQ
+$scope.changeCheckHEQ = function() {
+    if ($scope.info.heq_measles != 1 &&
+        $scope.info.heq_mumps != 1 &&
+        $scope.info.heq_meningitis != 1 &&
+        $scope.info.heq_pox != 1 &&
+        $scope.info.heq_scarlet_fever != 1 &&
+        $scope.info.heq_rheumantic != 1) {
+        $scope.info.heq_check = null;
+    }
+};
+//END
