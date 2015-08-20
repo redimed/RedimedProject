81498982b9d0e19c4afb7e0a2d1993d4cddfb628
luannm <luannguyen141194@gmail.com>
2015-08-20 10:31:49 +0700

    commit

--- Projects/redimedProjStruct/models/mdtApptPatient.js
+++ Projects/redimedProjStruct/models/mdtApptPatient.js
@@ -15,7 +15,18 @@ module.exports = function(sequelize, DataTypes){
       "actual_doctor_id" : DataTypes.INTEGER(11),
       "room_id" : DataTypes.INTEGER(11),
       "injury_id" : DataTypes.INTEGER(11),
-      "isPickUp" : DataTypes.INTEGER(11)
+      "isPickUp" : DataTypes.INTEGER(11),
+      "PHONE" : DataTypes.STRING(20),
+      "APP_TYPE" : DataTypes.STRING(10),
+      "STATUS" : DataTypes.STRING(20),
+      "ARR_TIME" : DataTypes.DATE,
+      "ATTEND_TIME" : DataTypes.DATE,
+      "AVAILABLE" : DataTypes.INTEGER(11),
+      "SERVICE_ID" : DataTypes.INTEGER(11),
+      "CLINICAL_DEPT_ID" : DataTypes.INTEGER(11),
+      "ACC_TYPE" : DataTypes.INTEGER(11),
+      "bill_to" : DataTypes.INTEGER(11),
+      "NOTES" : DataTypes.STRING(50)
     }, {
         tableName: "cln_appt_patients",
         createdAt: "Creation_date",
